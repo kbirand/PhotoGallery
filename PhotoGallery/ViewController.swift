ed5d990bc210f0347148fe7639cb6fbd719e4e2c
Koray Birand <info@koraybirand.com>
2018-09-29 11:01:42 +0300

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Koray Birand on 9/29/18.
+//  Copyright © 2018 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
